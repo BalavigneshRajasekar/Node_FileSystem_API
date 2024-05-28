f7a6a431f7a2826c985ab527c624aee08a15d9f9
BalavigneshRajasekar <you@example.com>
2024-05-28 20:48:58 +0530

    First end point added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fileSystem = require("fs-extra");
 const path = require("path");
-const data = require("./data.json");
+
 const bodyParser = require("body-parser");
 
 const server = express();
